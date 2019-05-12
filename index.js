f045283941b58b70b0837f6917b367c7f04f16cb
yklego <lego91003@gmail.com>
2019-05-12 20:10:12 +0800

    border reshapping and cancel recheck

--- index.js
+++ index.js
@@ -23,35 +23,31 @@ $(document).ready(function() {
     // $("#drinks .howmuch").text("$ "+??);
 
 
-    $("#main-meal").append(getDetail("xxx", "80"));
+    //---------------------------------------(2019.5.11) by Wu
+    //Recieve data from the menu page
+    // (number, name-of-main-meal, price)
+    $("#main-meal").append(getDetail(2, "xxx", "80"));
 
 });
 
 var mclick = 0;
 $("#main-meal .button").click(function() {
     if (mclick == 0) { //click once
-         $(".detail").toggleClass('showing-detail', 'slow');
+         $("#main-meal .detail").toggleClass('showing-detail', 'slow');
         mclick = 1;
     } else { //click twice        
-        $(".detail").toggleClass('showing-detail', 'slow');
+        $("#main-meal .detail").toggleClass('showing-detail', 'slow');                    
         mclick = 0;
     }
 });
 
 var sclick = 0;
 $("#side-meal .button").click(function() {
-    if (sclick == 0) { //click once
-        //$("").insertAfter("#side-meal");
-        $("#drinks").animate({
-            marginTop: "30px",
-            marginBottom: "0px",
-        }, 300);
+    if (sclick == 0) { //click once      
+        $("#side-meal .detail").toggleClass('showing-detail', 'slow');
         sclick = 1;
     } else { //click twice
-        $("#drinks").animate({
-            marginTop: "0px",
-            marginBottom: "-8px",
-        }, 300);
+        $("#side-meal .detail").toggleClass('showing-detail', 'slow');
         sclick = 0;
     }
 });
@@ -60,26 +56,33 @@ var dclick = 0;
 $("#drinks .button").click(function() {
     $("").insertAfter("#drinks");
     if (dclick == 0) { //click once
-
+        $("#drinks .detail").toggleClass('showing-detail', 'slow');
         dclick = 1;
     } else { //click twice
-
+        $("#drinks .detail").toggleClass('showing-detail', 'slow');
         dclick = 0;
     }
 });
 
-function removeThis() {
-    console.log("why");
-    $(".close").closest(".detail").hide(500, function() {
-        $(".close").closest(".detail").remove();
-    });
 
+var temp;
+function ReallyDelete(thisObj){
+    $('.ui.basic.modal').modal('show');
+    temp = thisObj
 }
 
-function getDetail(name, money) {
+function removeThis(){       
+        temp.parent(".detail").transition("fade right", function() {
+            thisObj.parent(".detail").remove();
+        });
+}
+
+
+
+function getDetail(n, name, money) {
     var s = "";
-    s = s + '<div class="detail" style="display:none">';
-    s = s + '   <p class="close" onclick="removeThis()">✖</p>';
+    s = s + '<div class="detail" >';
+    s = s + '   <p class="close" onclick="ReallyDelete($(this))">✖</p>';
     s = s + '   <p class="name">' + name + '</p>';
     s = s + '   <label class="check-container">不要洋蔥';
     s = s + '       <input type="checkbox">';
@@ -91,5 +94,5 @@ function getDetail(name, money) {
     s = s + '   </label>';
     s = s + '   <p class="howmuch">' + "$ " + money + '</p>';
     s = s + '</div>';
-    return s;
+    return s.repeat(n);
 }
